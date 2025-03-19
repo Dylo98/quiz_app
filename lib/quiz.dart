743ee15bf2a7edbb17c968033b69730c74692c92
Dylo98 <dawid.dyl@microsoft.wsei.edu.pl>
2025-03-19 21:27:02 +0100

    Map question

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -44,7 +44,7 @@ class _QuizState extends State<Quiz> {
     }
 
     if (activeScreen == 'result-screen') {
-      screenWidget = ResultScreen();
+      screenWidget = ResultScreen(chosenAnswers: selectedAnswers);
     }
 
     return MaterialApp(
